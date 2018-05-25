f59fc438f3bf90bdcc2d0f3138735dae36a96f49
demensdeum <demensdeum@gmail.com>
2018-05-25 10:40:54 +0400

    Changes, namespaces

---
+++ Const/Const.h
@@ -0,0 +1,17 @@
+#ifndef FSEGTAConst_defined
+#define FSEGTAConst_defined
+
+namespace FlameSteelEngine {
+namespace GameToolkit {
+namespace Algorithms {
+
+const string ConstMapGameplayEntityClass = "Map Gameplay Entity";
+
+const string ConstMapEntityStartPoint = "Map start point";
+const string ConstMapEntityEndPoint = "Map end point";
+
+};
+};
+};
+
+#endif
--- Const/FSEGTAConst.h
+++
@@ -1,9 +0,0 @@
-#ifndef FSEGTAConst_defined
-#define FSEGTAConst_defined
-
-const string FSEGTAConstMapGameplayEntityClass = "Map Gameplay Entity";
-
-const string FSEGTAConstMapEntityStartPoint = "Map start point";
-const string FSEGTAConstMapEntityStartPoint = "Map end point";
-
-#endif
